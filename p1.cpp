36a609ff48cb07d36369a3d162ed923d2e50ab75
HenriqueProj <henrique.caroco3@gmail.com>
2022-12-08 16:48:04 +0000

    COMPILAAA

--- p1.cpp
+++ p1.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <functional>
 
-int N, M;
+int N, M; // Num linhas, N colunas
+int combinacoes;
 
 std::vector<int> lines_limits;
 std::vector<std::vector<int>> grid;
@@ -54,6 +57,70 @@ int MaxSquare() {
         return M;
     }
 }
+
+bool canBuildSquare(int x, int y, int size, std::vector<std::vector<int>> grid_){
+    if(x + 1 == size || y+1 == size || size > MaxSquare() )
+        return false;
+
+    if(x > lines_limits[y - size])
+        return false;
+
+    return true;
+}
+
+// Finished 
+std::vector<std::vector<int>> buildSquare(int x, int y, int size, std::vector<int> limites_linhas, std::vector<std::vector<int>> grid_){
+    std::vector<std::vector<int>> new_grid = grid_;
+    
+    for(int i = 0; i < size; i++)
+        for(int j = 0; j < size; j++)
+            grid_[x-i][j-i]++;
+
+    return grid_;
+}
+
+// Finished 
+std::vector<int> decreaseLimit(std::vector<int> limites_linhas, int size){
+    int cont = 0;
+
+    for (auto it = limites_linhas.rbegin(); it != limites_linhas.rend(); it++){
+        if(cont < size && *it != 0){
+            it -= size;
+            cont++;
+        }
+        else
+            return limites_linhas;
+    }
+    return limites_linhas;
+}
+
+
+void solve(int x, int y, std::vector<int> limites_linhas, std::vector<std::vector<int>> grid_){
+
+    if ( std::adjacent_find( limites_linhas.begin(), limites_linhas.end(), std::not_equal_to<int>() ) == limites_linhas.end() ){
+        combinacoes++;
+        return;
+    }
+
+    if(x > limites_linhas[y])
+        solve(x-1, y, limites_linhas, grid_);
+
+    //Corpo da função
+
+    if(1){
+
+    }
+
+    //
+
+    if(!x){
+        if(y)
+            solve(limites_linhas[y - 1] - 1, y -1, limites_linhas, grid_);
+    }
+    else
+        solve(x-1, y, limites_linhas, grid_);
+}
+
 int main() {
     getInput();
     initiateGrid();
